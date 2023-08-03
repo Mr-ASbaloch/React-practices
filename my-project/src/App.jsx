c3a4d9c914b9410a4583ccece546098be0378b87
abdul saeed <abdulsaeed1356@gmail.com>
2023-08-03 12:49:32 +0500

    work done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 // import './App.css'
+import './index.css'
 
 function App() {
  
 
   return (
     <>
-     <h1 >hello world</h1>
-        
-    
+     <h1 className="text-3xl font-bold underline  text-emerald-400 ">
+      Hello world!
+    </h1>
     </>
   )
 }
