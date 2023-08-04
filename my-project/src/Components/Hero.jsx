a208ce279bbe0ed4f5a1b6c8a841aab3af9aabf7
abdul saeed <abdulsaeed1356@gmail.com>
2023-08-04 11:11:15 +0500

    added

--- my-project/src/Components/Hero.jsx
+++ my-project/src/Components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
          <p>Foods <span>Delivered</span> </p>
       </div>
       <div>
-        <img
+        <img className=" w-[300px] h-full  bg-slate-500 "
           src="https://images.pexels.com/photos/8609973/pexels-photo-8609973.jpeg?auto=compress&cs=tinysrgb&w=600"
           alt="img"
         />
