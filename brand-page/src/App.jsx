05c9441730ce0e8340550b8d5ab413a0830a3296
abdul saeed <abdulharoon1356@gmail.com>
2023-08-08 22:36:52 -0700

    commit

--- brand-page/src/App.jsx
+++ brand-page/src/App.jsx
@@ -8,7 +8,10 @@ function App() {
 
   return (
     <>
-     <h1 >hello world</h1>
+     <h1>
+      
+      
+      hello world</h1>
         
     
     </>
--- landing-page/src/App.jsx
+++ landing-page/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
      
-     <h1 className="text-3xl font-bold underline">
+     <h1 className="text-3xl font-bold underline text-center">
       Hello world!
     </h1>
     </>
